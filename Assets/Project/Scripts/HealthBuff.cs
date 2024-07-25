348f51ffadf63238c535f54a97c463f6785cac5b
Cleoir Canei <canei.opet@hotmail.com>
2024-07-25 18:40:54 -0300

    development shoting e create hud

--- Assets/Project/Scripts/HealthBuff.cs
+++ Assets/Project/Scripts/HealthBuff.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Powerups/HealthBuff")]
 public class HealthBuff : PowerUpEffect
 {
-    [Range(2, 3)]
+    [Range(1, 3)]
     public int amount;
+
+    private int valueDefault = 1;
 
     public override void Apply(GameObject target)
     {
-        int life = target.GetComponent<PlayerControl>().life;
-        if (life < amount)
+        var player = target.GetComponent<PlayerControl>();
+        int life = player.life;
+
+        if (life == player.maxLife && amount == player.maxLife)
+        {
+            target.GetComponent<PlayerControl>().SetBarStars(valueDefault);
+        }
+        else if (life == player.maxLife && amount <= player.maxLife)
+        {
+            target.GetComponent<PlayerControl>().SetPoints(amount);
+        }
+        else if (life < amount)
         {
             target.GetComponent<PlayerControl>().SetBarLife(amount);
+        }
+        else
+        {
+            life++;
+            target.GetComponent<PlayerControl>().SetBarLife(life);
         }
     }
 }
